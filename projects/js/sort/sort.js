654c9580384d4a7282ed5812b3ff14e5b3086995
Jarrett Meyer <meyer_jarrett@lilly.com>
2018-11-26 09:52:52 -0500

    Updated sorting demo.

--- projects/js/sort/sort.js
+++ projects/js/sort/sort.js
@@ -1,28 +1,29 @@
 (function () {
   
-  const titleFont = "monospace";
-  const titleFontSize = 12;
-  const titleHeight = 50;
-  const count = 50;
-  const barColor = "#d7d7d7";
-  const readColor = "#c83f3f"
-  const writeColor = "#1f8e1b";
+    const TITLE_FONT = "monospace";
+    const TITLE_FONT_SIZE = 12;
+    const TITLE_HEIGHT = 50;
+    const COUNT = 200;
+    const BAR_COLOR = "lightgray";
+    const READ_COLOR = "mediumorchid"
+    const WRITE_COLOR = "steelblue";
+    const DELAY_INTERVAL = 1;
   
-  function draw() {
-    let bars = [];
-    for (let i = 0; i < count; i++) {
-      let bar = drawArea.append("rect")
-          .attr("x", scaleX(i))
-          .attr("y", scaleY(1 - randoms[i]))
-          .attr("width", barWidth)
-          .attr("height", scaleY(randoms[i]))
-          .attr("index", i)
-          .attr("value", randoms[i])
-          .attr("fill", barColor);
-      bars.push(bar);
+    function draw() {
+        let bars = [];
+        for (let i = 0; i < COUNT; i++) {
+            let bar = drawArea.append("rect")
+                .attr("x", scaleX(i))
+                .attr("y", scaleY(1 - randoms[i]))
+                .attr("width", barWidth)
+                .attr("height", scaleY(randoms[i]))
+                .attr("index", i)
+                .attr("value", randoms[i])
+                .attr("fill", BAR_COLOR);
+            bars.push(bar);
+        }
+        return bars;
     }
-    return bars;
-  }
   
   function generateRandomData(n) {
     let arr = [];
@@ -32,61 +33,60 @@
     return arr;
   }
   
-  function readBar(bar) {
-    bar.attr("fill", readColor);
-    return +bar.attr("value");
-  }
+    function readBar(bar) {
+        bar.attr("fill", READ_COLOR);
+        return +bar.attr("value");
+    }
   
-  function writeBar(bar) {
-    bar.attr("fill", writeColor);
-  }
+    function writeBar(bar) {
+        bar.attr("fill", WRITE_COLOR);
+    }
   
-  function doneBar(bar) {
-    bar.attr("fill", barColor);
-  }
+    function doneBar(bar) {
+        bar.attr("fill", BAR_COLOR);
+    }
   
-  function swapXPosition(barA, barB) {
-    writeBar(barA);
-    writeBar(barB);
-    a = +barA.attr("x");
-    b = +barB.attr("x");
-    barA.attr("x", b);
-    barB.attr("x", a);
-  }
+    function swapXPosition(barA, barB) {
+        let a = +barA.attr("x");
+        let b = +barB.attr("x");
+        barA.attr("x", b);
+        barB.attr("x", a);
+    }
   
-  function bubbleSort(bars) {
-    for (let i = 0; i < bars.length - 1; i++) {
-      for (let j = 0; j < bars.length - i - 1; j++) {
+    let canvas = d3.select("#canvas");
+    let width = +canvas.attr("width");
+    let height = +canvas.attr("height");
+    let barHeight = height - TITLE_HEIGHT;
+    let barWidth = width / COUNT;
+  
+    let scaleX = d3.scaleLinear().domain([0, COUNT]).range([0, width]);
+    let scaleY = d3.scaleLinear().domain([0, 1]).range([0, barHeight]);
+  
+    let randoms = generateRandomData(COUNT);
+  
+    let drawArea = canvas.append("g").attr("transform", `translate(0, ${TITLE_HEIGHT})`);
+  
+    let bars = draw();
+    let i = j = 0;
+    let interval = setInterval(function () {
         let val1 = readBar(bars[j]);
         let val2 = readBar(bars[j + 1]);
         if (val1 > val2) {
-          let swap = bars[j];
-          bars[j] = bars[j + 1];
-          bars[j + 1] = swap;
-          swapXPosition(bars[j], bars[j + 1]);
+            let swap = bars[j];
+            bars[j] = bars[j + 1];
+            bars[j + 1] = swap;
+            writeBar(bars[j]);
+            writeBar(bars[j + 1]);
+            swapXPosition(bars[j], bars[j + 1]);
         }
-        // doneBar(bars[j]);
-        // doneBar(bars[j + 1]);
-      }
-    }
-  }
-  
-  let canvas = d3.select("#canvas");
-  let width = +canvas.attr("width");
-  let height = +canvas.attr("height");
-  let barHeight = height - titleHeight;
-  let barWidth = width / count;
-  
-  let scaleX = d3.scaleLinear().domain([0, count]).range([0, width]);
-  let scaleY = d3.scaleLinear().domain([0, 1]).range([0, barHeight]);
-  
-  let randoms = generateRandomData(count);
-  
-  let drawArea = canvas.append("g").attr("transform", `translate(0, ${titleHeight})`);
-  
-  let bars = draw();
-  setTimeout(function () {
-    bubbleSort(bars);
-  }, 1000);
+        j++;
+        if (j >= bars.length - i - 1) {
+            i++;
+            j = 0;
+        }
+        if (i >= bars.length - 1) {
+            clearInterval(interval);            
+        }
+    }, DELAY_INTERVAL);
   
 })();
