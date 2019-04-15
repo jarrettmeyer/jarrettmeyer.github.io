1a4d9016aec00f159f780361f4ff2caec78548c6
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-04-15 07:47:48 -0400

    Adds history portion of Seder presentation

---
+++ presentations/seder/seder.js
@@ -0,0 +1,8 @@
+Reveal.initialize({
+    controles: true,
+    hash: true,
+    history: true,
+    progress: true,
+    slideNumber: true,
+    transition: "convex", // none/fade/slide/convex/concave/zoom
+});
