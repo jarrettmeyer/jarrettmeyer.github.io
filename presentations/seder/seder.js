c17bec843399ddb898def2aefec834f2c5bb69ce
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-04-17 08:38:00 -0400

    updated formatting

--- presentations/seder/seder.js
+++ presentations/seder/seder.js
@@ -1,9 +1,9 @@
 Reveal.initialize({
-    controls: true,
-    fragmentInURL: true,
-    hash: true,
-    history: true,
-    progress: true,
-    slideNumber: true,
-    transition: "convex", // none/fade/slide/convex/concave/zoom
+    controls:       true,
+    fragmentInURL:  true,
+    hash:           true,
+    history:        true,
+    progress:       true,
+    slideNumber:    true,
+    transition:     "convex", // none/fade/slide/convex/concave/zoom
 });
