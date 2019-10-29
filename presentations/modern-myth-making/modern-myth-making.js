ba93299bdb117be27e66bcea6a076459bd9da297
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-10-29 16:20:17 -0400

    Work on MMM and tikun olam

---
+++ presentations/modern-myth-making/modern-myth-making.js
@@ -0,0 +1,9 @@
+Reveal.initialize({
+    controls:       true,
+    fragmentInURL:  true,
+    hash:           true,
+    history:        true,
+    progress:       true,
+    slideNumber:    true,
+    transition:     "convex", // none/fade/slide/convex/concave/zoom
+});
