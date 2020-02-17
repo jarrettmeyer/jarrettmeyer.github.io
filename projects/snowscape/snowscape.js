bc7762c66014f40f176a5357f73d2d93e7db1efc
Jarrett Meyer <meyer_jarrett@lilly.com>
2020-02-16 19:20:40 -0500

    remove d3 from snowscape

--- projects/snowscape/snowscape.js
+++ projects/snowscape/snowscape.js
@@ -1,26 +1,11 @@
-const body = document.body;
-const width = body.clientWidth;
-const height = body.clientHeight;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-const maxSnowflakeCount = 120;
-const minDuration = 4000;
-const maxDuration = 12000;
-const minRadius = 2;
-const maxRadius = 5;
-
-const moonGradientId = "moon-gradient";
-const moonInnerRadius = Math.min(0.2 * width, 0.1 * height);
-const moonOuterRadius = moonInnerRadius * 1.05;
-const moonColor = "#fffce9";
-const moonInitialPos = {
-    x: rand(0.4, 0.8) * width,
-    y: rand(0.05, 0.2) * height,
-};
-
-const snowBlurStdDeviation = 3;
-const snowBlurId = "snow-blur";
-const groundBlurStdDeviation = 2;
-const groundBlurId = "ground-blur";
+const snowscape = document.getElementById("snowscape");
+const nightSky = document.getElementById("night-sky");
+const ground = document.getElementById("ground");
+const moon = document.getElementById("moon");
+const gSnowflakes = document.getElementById("snowflakes");
 
 /**
  * Randomly generate a value between `min` and `max`.
@@ -31,146 +16,106 @@ const groundBlurId = "ground-blur";
  */
 function rand(min, max) {
     const range = max - min;
-    return min + range * Math.random();
+    const value = min + range * Math.random();
+    return value;
 }
 
-const svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [0, 0, width, height]);
+// Update the dimensions of the snowscape
+snowscape.setAttribute("width", width);
+snowscape.setAttribute("height", height);
+snowscape.setAttribute("viewBox", `0 0 ${width} ${height}`);
 
-const defs = svg.append("defs");
+// Update the dimensions of the night sky
+nightSky.setAttribute("width", width);
+nightSky.setAttribute("height", height);
 
-const skyLinearGradient = defs.append("linearGradient")
-    .attr("id", "sky-gradient")
-    .attr("gradientTransform", "rotate(90)");
-skyLinearGradient.append("stop")
-    .attr("offset", "0%")
-    .attr("stop-color", "#07043f");
-skyLinearGradient.append("stop")
-    .attr("offset", "100%")
-    .attr("stop-color", "#2f2b72");
+// Move the moon
+const moonPositionX = Math.round(rand(0.3 * width, 0.7 * width));
+const moonPositionY = Math.round(rand(0.1 * height, 0.1 * height));
+const moonRotation = Math.round(rand(10, 60));
+moon.setAttribute("transform", `translate(${moonPositionX}, ${moonPositionY}) rotate(${moonRotation})`);
 
-const snowFilter = defs.append("filter")
-    .attr("id", snowBlurId)
-    .attr("x", "-100%")
-    .attr("y", "-100%")
-    .attr("width", "300%")
-    .attr("height", "300%");
-snowFilter.append("feGaussianBlur")
-    .attr("stdDeviation", snowBlurStdDeviation);
+// Create the path object for the ground
+const numPoints = 8;
+const deltaX = Math.ceil(width / numPoints);
+let prevX = -deltaX;
+let prevY = rand(0.8 * height, 0.9 * height);
+let controlX = controlY = 0;
+let groundPath = `M ${prevX} ${height}`;
+groundPath += `L ${prevX} ${prevY}`;
+for (let i = 0; i < numPoints + 1; i++) {
+    nextX = i * deltaX;
+    nextY = prevY - Math.round(rand(-0.02 * height, 0.02 * height));
+    controlX = Math.round((prevX + nextX) / 2);
+    controlY = Math.round((prevY + nextY) / 2);
+    groundPath += `S ${controlX} ${controlY} ${nextX} ${nextY}`;
+    prevX = nextX;
+    prevY = nextY;
+}
+groundPath += `L ${width + deltaX} ${height}`;
+groundPath += "Z";
+ground.setAttribute("d", groundPath);
 
-const groundFilter = defs.append("filter")
-    .attr("id", groundBlurId)
-    .attr("x", "-100%")
-    .attr("y", "-100%")
-    .attr("width", "300%")
-    .attr("height", "300%");
-groundFilter.append("feGaussianBlur")
-    .attr("stdDeviation", groundBlurStdDeviation);
+const numSnowflakes = 120;
+const pCreateSnowflake = 0.05;
+const snowflakes = new Array(numSnowflakes);
 
-svg.append("rect")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", "url(#sky-gradient)");
+// Create an animation loop for snow falling.
+function snowfall(snowflake) {
+    // Get properties about the snowflake
+    const index = +snowflake.dataset.index;
+    const createdAt = +snowflake.dataset.createdAt;
+    const duration = +snowflake.dataset.duration;
+    const timer = +snowflake.dataset.timer;
+    const cx = +snowflake.getAttribute("cx");
+    const r = +snowflake.getAttribute("r");
+    return (now) => {
+        // Update the snowflake's properties
+        const elapsed = now - createdAt;
+        const cy = elapsed / duration * height;
+        snowflake.setAttribute("cy", cy);
+        snowflake.setAttribute("cx", cx + rand(-0.1, 0.1));
+        snowflake.setAttribute("r", r + rand(-0.1, 0.1));
+        if (elapsed < duration) {
+            // Still ticking...
+            window.requestAnimationFrame(snowfall(snowflake));
+        } else {
+            // Destroy the snowflake and cancel the animation frame
+            snowflakes[index] = undefined;
+            snowflake.remove();
+            window.cancelAnimationFrame(timer);
+        }
+    };
+}
 
-const groundLineGenerator = d3.line()
-    .x(d => d.x)
-    .y(d => d.y)
-    .curve(d3.curveBasisClosed);
+setInterval(() => {
+    for (i = 0; i < numSnowflakes; i++) {
+        if (!snowflakes[i] && Math.random() < pCreateSnowflake) {
+            // Set up the timing for the snowflake
+            const createdAt = performance.now();
+            const duration = rand(5000, 15000);
 
-const groundLine = groundLineGenerator([
-    {
-        x: -0.2 * width,
-        y: 1.2 * height,
-    },
-    {
-        x: -0.2 * width,
-        y: 0.8 * height,
-    },
-    {
-        x: rand(0.0, 0.2) * width,
-        y: rand(0.7, 0.9) * height,
-    },
-    {
-        x: rand(0.2, 0.4) * width,
-        y: rand(0.7, 0.9) * height,
-    },
-    {
-        x: rand(0.4, 0.6) * width,
-        y: rand(0.7, 0.9) * height,
-    },
-    {
-        x: rand(0.6, 0.8) * width,
-        y: rand(0.7, 0.9) * height,
-    },
-    {
-        x: rand(0.8, 1.0) * width,
-        y: rand(0.7, 0.9) * height,
-    },
-    {
-        x: 1.2 * width,
-        y: 0.8 * height,
-    },
-    {
-        x: 1.2 * width,
-        y: 1.2 * height,
+            // Create the snowflake object
+            const snowflake = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            snowflake.dataset.index = i;
+            snowflake.dataset.createdAt = createdAt;
+            snowflake.dataset.duration = duration;
+            snowflake.setAttribute("cx", rand(0, width));
+            snowflake.setAttribute("cy", 0);
+            snowflake.setAttribute("r", rand(1, 5));
+            snowflake.setAttribute("stroke", "none");
+            snowflake.setAttribute("fill", "#ffffff");
+            snowflake.setAttribute("filter", "url(#snow-blur)");
+
+            // Add the snowflake to the screen.
+            gSnowflakes.append(snowflake);
+
+            // Add the snowflake to the collection
+            snowflakes[i] = snowflake;
+
+            // Create an animation frame for the snowflake and save the timer handle
+            const timer = window.requestAnimationFrame(snowfall(snowflake));
+            snowflake.dataset.timer = timer;
+        }
     }
-]);
-
-const ground = svg.append("path")
-    .classed("ground", true)
-    .attr("d", groundLine)
-    .attr("stroke", "none")
-    .attr("fill", "#ffffff")
-    .attr("filter", `url(#${groundBlurId})`);
-
-const moonPath = `M 0 0
-    A ${moonOuterRadius} ${moonOuterRadius} 0 1 1 0 ${2 * moonInnerRadius}
-    A ${moonInnerRadius} ${moonInnerRadius} 0 0 0 0 0
-    Z`;
-
-const moon = svg.append("g")
-    .attr("transform", `translate(${moonInitialPos.x}, ${moonInitialPos.y}) rotate(${rand(10, 50)})`)
-    .append("path")
-    .classed("moon", true)
-    .attr("d", moonPath)
-    .attr("stroke", "none")
-    .attr("fill", moonColor)
-    .attr("filter", `url(#${snowBlurId})`);
-
-const snowflakes = new Array(maxSnowflakeCount)
-    .fill(0)
-    .map((_, i) => { return { index: i }; });
-
-function loop() {
-    d3.active(this)
-        .attr("cy", height)
-        .transition()
-        .duration(1)
-        .attr("cx", () => rand(0, width))
-        .attr("cy", -10)
-        .attr("r", () => rand(minRadius, maxRadius))
-        .transition()
-        .duration(() => rand(minDuration, maxDuration))
-        .ease(d3.easeLinear)
-        .on("start", loop);
-}
-
-
-const snow = svg.selectAll("circle.snowflake")
-    .data(snowflakes)
-    .join("circle")
-    .classed("snowflake", true)
-    .attr("cx", () => rand(0, width))
-    .attr("cy", () => rand(-height, 0))
-    .attr("r", () => rand(minRadius, maxRadius))
-    .attr("stroke", "none")
-    .attr("fill", "#ffffff")
-    .attr("filter", `url(#${snowBlurId})`)
-    .transition()
-    .duration(() => rand(2 * minDuration, 2 * maxDuration))
-    .ease(d3.easeLinear)
-    .on("start", loop);
+}, 100);
