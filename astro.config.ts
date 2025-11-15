40f9b437691d476d6a595c6c0d7a7387d7c95876
Jarrett Meyer <jarrettmeyer@gmail.com>
2025-11-15 09:11:51 -0500

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jarrettmeyer.com",
-  integrations: [expressiveCode()],
+  integrations: [expressiveCode(), sitemap()],
 });
