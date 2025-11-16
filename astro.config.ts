8feb6c1474f5331faa9d3b331e2aaa15858dde13
Jarrett Meyer <jarrettmeyer@gmail.com>
2025-11-15 19:05:50 -0500

    add astro-pagefind

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
 import expressiveCode from "astro-expressive-code";
+import pagefind from "astro-pagefind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jarrettmeyer.com",
-  integrations: [expressiveCode(), mdx(), sitemap()],
+  integrations: [expressiveCode(), mdx(), pagefind(), sitemap()],
 });
