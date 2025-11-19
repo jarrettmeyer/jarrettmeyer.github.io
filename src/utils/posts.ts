29aa7c4288f219f8b4666157b038e4a96d546c34
Jarrett Meyer <meyer_jarrett@lilly.com>
2025-11-18 21:01:44 -0500

    feat: refactor post filtering and retrieval logic; introduce utility functions for better code organization

---
+++ src/utils/posts.ts
@@ -0,0 +1,56 @@
+import { getCollection } from "astro:content";
+import { slugifyTag } from "./slugify";
+import type { Post } from "@/types";
+
+export const filterByCount = (count?: number) => {
+  return (_post: Post, index: number) => {
+    if (count && count > 0) {
+      return index < count;
+    }
+    return true;
+  };
+}; 
+
+export const filterByDate = (date?: Date) => {
+  date = date || new Date();
+  return (post: Post) => {
+    if (import.meta.env.PROD && new Date(post.data?.date) >= date) {
+      return false;
+    }
+    return true;
+  };
+}; 
+
+export const filterByDraft = () => {
+  return (post: Post) => {
+    if (import.meta.env.PROD && post.data?.draft) {
+      return false;
+    }
+    return true;
+  };
+};
+
+export const filterByTag = (tag?: string) => {
+  return (post: Post) => {
+    if (tag) {
+      if (tag === "uncategorized") {
+        return !(post.data?.tags && post.data.tags.length > 0);
+      }
+      return (post.data?.tags ?? []).some((t: string) => slugifyTag(t) === tag);
+    }
+    return true;
+  };
+};
+
+export const sortPostsByDate = () => {
+  return (a: Post, b: Post) => {
+    const bDate = b.data?.date?.valueOf?.() ?? 0;
+    const aDate = a.data?.date?.valueOf?.() ?? 0;
+    return bDate - aDate;
+  };
+};
+
+/**
+ * All posts in the collection.
+ */
+export const getAllPosts = () => getCollection("posts");
