2ffc321673147772591097d096188cd5b71d310a
Jarrett Meyer <meyer_jarrett@lilly.com>
2026-04-07 17:53:36 -0400

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://jarrettmeyer.com',
 	integrations: [mdx(), sitemap()],
 });
