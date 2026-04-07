f75665d4dbea807ade0ce6ddb71b05cdf3818533
Jarrett Meyer <meyer_jarrett@lilly.com>
2026-04-07 19:02:23 -0400

    Add light/dark toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import externalLinkDecorator from './src/plugins/external-link-decorator';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jarrettmeyer.com',
-	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [externalLinkDecorator],
+	},
+	integrations: [mdx({ rehypePlugins: [externalLinkDecorator] }), sitemap()],
 });
