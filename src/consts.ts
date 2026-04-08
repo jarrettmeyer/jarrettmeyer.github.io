0535a8441dcf31e93a6f24b0c4b8bca921168031
Jarrett Meyer <meyer_jarrett@lilly.com>
2026-04-08 15:24:02 -0400

    homepage updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Jarrett Meyer";
+/**
+ * Number of milliseconds per year.
+ */
+export const MS_PER_YEAR = 365.25 * 24 * 60 * 60 * 1000;
+
 export const SITE_DESCRIPTION =
   "Jarrett Meyer writes about software architecture, ML engineering, and using the right tool — not the trendy one.";
+
+export const SITE_TITLE = "Jarrett Meyer";
