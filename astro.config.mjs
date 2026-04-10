ce90cd0a8201e0ed0c69c88059829fdd011326cb
Jarrett Meyer <meyer_jarrett@lilly.com>
2026-04-10 10:11:44 -0400

    New post: Every Database Textbook Is Wrong
    - Add Mermaid diagram support
    - Add hero image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,25 @@ import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import externalLinkDecorator from "./src/plugins/external-link-decorator";
+import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jarrettmeyer.com",
   markdown: {
-    rehypePlugins: [externalLinkDecorator],
+    rehypePlugins: [externalLinkDecorator, rehypeMermaid],
+    syntaxHighlight: {
+      excludeLangs: ["mermaid"],
+    },
   },
   integrations: [
-    mdx({ rehypePlugins: [externalLinkDecorator] }),
+    mdx({
+      rehypePlugins: [
+        externalLinkDecorator,
+        [rehypeMermaid, { strategy: "img-svg" }],
+      ],
+      syntaxHighlight: { excludeLangs: ["mermaid"], type: "shiki" },
+    }),
     sitemap(),
     partytown({
       config: { forward: ["dataLayer.push", "gtag"] },
